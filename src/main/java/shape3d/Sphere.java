4d2bbdb38f7e4a750a1bf7183d4ac547ac7658e0
abo781 <philipp.jenke@haw-hamburg.de>
2022-09-29 11:43:16 +0200

    Improved sphere implementation.

--- src/main/java/shape3d/Sphere.java
+++ src/main/java/shape3d/Sphere.java
@@ -13,31 +13,36 @@ import java.util.List;
  * A sphere 3D shape.
  */
 public class Sphere implements Shape3D {
-  private Vector3f center;
-  private float radius;
+    private Vector3f center;
+    private float radius;
 
-  public Sphere(Vector3f center, float radius) {
-    this.center = new Vector3f(center);
-    this.radius = radius;
-  }
+    public Sphere(Vector3f center, float radius) {
+        this.center = new Vector3f(center);
+        this.radius = radius;
+    }
 
-  public Vector3f getCenter() {
-    return center;
-  }
+    public Vector3f getCenter() {
+        return center;
+    }
 
-  public float getRadius() {
-    return radius;
-  }
+    public float getRadius() {
+        return radius;
+    }
 
-  /**
-   * Returns true if the point p is inside the sphere.
-   */
-  public boolean isInside(Vector3f p) {
-    return p.subtract(center).lengthSquared() <= radius * radius;
-  }
+    /**
+     * Returns true if the point p is inside the sphere.
+     */
+    public boolean isInside(Vector3f p) {
+        return p.subtract(center).lengthSquared() <= radius * radius;
+    }
 
-  @Override
-  public List<Shape3D> intersect(Shape3D other) {
-    return Intersector3D.intersectSphereWith(this, other);
-  }
+    @Override
+    public List<Shape3D> intersect(Shape3D other) {
+        return Intersector3D.intersectSphereWith(this, other);
+    }
+
+    @Override
+    public String toString() {
+        return "center: " + center.toString() + ", radius: " + radius;
+    }
 }
